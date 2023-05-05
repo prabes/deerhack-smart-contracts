9555ffafb30f18f4f2d66f22335029eafa776cd8
prabes <dprabesh11@gmail.com>
2023-05-06 02:08:49 +0545

    Add all retrival function

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -8,7 +8,7 @@ contract PatentContract {
   struct Patent {
     uint256 id;
 		string description;
-		address currentOrder;
+		address currentOwner;
 		address newOwner;
 		bool buyerSigned;
 		bool sellerSigned;
@@ -18,6 +18,7 @@ contract PatentContract {
   }
 	
 	mapping(uint256 => Patent) public patents;
+
 	uint256 public patentCount;
 
 	function validatePatentData (string memory _description, address _currentOwner, string memory _ipfsHash) public {
@@ -32,5 +33,83 @@ contract PatentContract {
 		require(patents[_id].status == PatentStatus.Pending, "Patent data is already validated");
 		// approve current patent 
 		patents[_id].status = PatentStatus.Approved;
+		// #TODO Add proper authorization for government roles:
+		patents[_id].govermentSigned = true;
+	}
+
+	function rejectPatentData (uint256 _id) public {
+		require(patents[_id].id != 0, "Data ID does not exist"); // check if ID does exist
+		require(patents[_id].status == PatentStatus.Pending, "Patent data is already validated");  //check if data is validated
+		patents[_id].status = PatentStatus.Rejected;  // approve current patent 
+		patents[_id].govermentSigned = true; 		// #TODO Add proper authorization for government roles
+	}
+
+	// retrive all patents
+ 	function getAllPatents() public view returns (Patent[] memory) {
+  	Patent[] memory result = new Patent[](patentCount);
+        for (uint256 i = 1; i <= patentCount; i++) {
+            result[i - 1] = patents[i];
+        }
+        return result;
+    }
+	
+	// retrive all approved patents
+	function getApprovedPatents() public view returns (Patent[] memory) {
+        Patent[] memory result = new Patent[](patentCount);
+        uint256 counter = 0;
+        for (uint256 i = 1; i <= patentCount; i++) {
+            if (patents[i].status == PatentStatus.Approved) {
+                result[counter] = patents[i];
+                counter++;
+            }
+        }
+        //remove if there is any unused elements
+        assembly {
+            mstore(result, counter)
+        }
+        return result;
+    } 
+
+	// retrive all rejected patents
+	function getRejectedPatents() public view returns (Patent[] memory) {
+		Patent[] memory result = new Patent[](patentCount);
+    	uint256 counter = 0;
+
+      for (uint256 i = 1; i <= patentCount; i++) {
+        if (patents[i].status == PatentStatus.Rejected) {
+          result[counter] = patents[i];
+            counter++;
+          }
+        }
+        // remove if there is any unused elements
+        assembly {
+            mstore(result, counter)
+        }
+        return result;
+    }
+
+	// retrive patent of specific Id
+	function getPatentById(uint256 _id) public view returns (Patent memory) {
+    require(patents[_id].id != 0, "Data ID does not exist"); // check if id does exist
+    return patents[_id];
+		
+	}
+
+	// retrive patents of an specific user address
+	function getPatentByAddress() public view returns (Patent[] memory) {
+    Patent[] memory result = new Patent[](patentCount);
+    uint256 counter = 0;
+
+		for (uint256 i = 1; i <= patentCount; i++) {
+        if (patents[i].currentOwner == msg.sender) {
+          result[counter] = patents[i];
+            counter++;
+          }
+        }
+		// remove if there is any unused elements
+    assembly {
+    	mstore(result, counter)
+    }
+		return result;
 	}
 }
