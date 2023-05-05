818b1b4ccec1ee44e538b8580cb60b7bd944bafd
prabes <dprabesh11@gmail.com>
2023-05-05 21:57:07 +0545

    Add basic createPatent method on smart-contract

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract PatentContract {
+  struct Patent {
+    uint256 id;
+		string description;
+		address currentOrder;
+		address newOwner;
+		bool buyerSigned;
+		bool sellerSigned;
+		bool govermentSigned;
+		string ipfsHash;
+  }
+	
+	mapping(uint256 => Patent) public patents;
+	uint256 public patentCount;
+
+	function createPatent(string memory _description, address _currentOwner, string memory _ipfsHash ) public {
+		patentCount++;
+		patents[patentCount] = Patent(patentCount, _description, _currentOwner, address(0), false, false, false, _ipfsHash);
+	}
+
+
+  function hello() public pure returns (uint256) {
+    return 1;
+  }
 }
