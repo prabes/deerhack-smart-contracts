5055c26589df59b5502d38f3854796fc31ef846b
prabes <dprabesh11@gmail.com>
2023-05-05 19:58:46 +0545

    Setting up hardhatconfig for sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,17 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.9',
+    version: "0.8.9",
+    defaultNetwork: "Sepolia",
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: "https://eth-sepolia.g.alchemy.com/v2/o7P5GnPGynDJafBfv7VOVjij8O2FVhlY",
+        accounts: [
+          "697747e5b973889cdea4a10f146b28deee673cd6aca8e89c15f08d150cfa9079",
+        ],
+      },
+    },
     settings: {
       optimizer: {
         enabled: true,
